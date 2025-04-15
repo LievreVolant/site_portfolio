af01fc8724847d5c90f7429003adb3069bd0b55e
LievreVolant <156901075+LievreVolant@users.noreply.github.com>
2025-04-15 14:46:05 +0200

    affichage projets

--- public/js/projets.js
+++ public/js/projets.js
@@ -1,3 +1,6 @@
+let inner = document.getElementsByClassName('div-inner')[0];
+let listeLangages = [];
+
 fetch('public/js/projets.json')
     .then(response => {
         if (!response.ok) {
@@ -6,11 +9,46 @@ fetch('public/js/projets.json')
         return response.json();
     })
     .then(contenu => {
-        console.log(contenu);
-        
-    
-    
-    
+        let paire = 0;
+
+        for (let i = 0; i <= Object.keys(contenu).length; i++) {
+            let divLarge = document.createElement("div");
+
+            if (paire%2 == 0) {
+                divLarge.classList.add("div-boite-gauche-ordi");
+            } else {
+                divLarge.classList.add("div-boite-droite-ordi");
+            }
+
+            let img = document.createElement("img");
+            img.classList.add("img-projet");
+            img.src = contenu[i].visuel;
+
+            let divTexte = document.createElement("div");
+            divTexte.classList.add("div-contenu");
+
+            let divTexteTitre = document.createElement("div");
+            divTexteTitre.classList.add("titre");
+            let h2Titre = document.createElement("h2");
+            h2Titre.append(contenu[i].nom);
+            divTexteTitre.append(h2Titre);
+
+            let p = document.createElement("p");
+            p.append(contenu[i].desc);
+            divTexte.append(divTexteTitre);
+            divTexte.append(p);
+
+            if (paire%2 == 0) {
+                divLarge.append(img);
+                divLarge.append(divTexte);
+            } else {
+                divLarge.append(divTexte);
+                divLarge.append(img);
+            }
+
+            inner.append(divLarge);
+            paire++;
+        };
     })
 
 
