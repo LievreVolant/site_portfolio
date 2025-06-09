0ccc3c2e786e7c21582cb0986b23005419aec8fe
LievreVolant <156901075+LievreVolant@users.noreply.github.com>
2025-06-10 00:32:56 +0200

    ajout des tags en dessous dans la page template projet

--- public/js/template_projet.js
+++ public/js/template_projet.js
@@ -1,6 +1,7 @@
 let params = new URLSearchParams(document.location.search);
 let num_projet = params.get("projet");
 
+let div_pres_projet = document.getElementById('div_pres_projet');
 let nom_projet = document.getElementById('nom_projet');
 let desc_projet = document.getElementById('desc_projet');
 let img_projet = document.getElementById('img_projet');
@@ -22,6 +23,17 @@ fetch('public/js/projets.json')
         desc_projet.innerHTML = projet.desc;
         contribution.innerHTML = projet.contribution;
 
+        let divTags = document.createElement("div");
+        divTags.classList.add("div-tags");
+        for (let j = 0; j < Object.keys(projet.langages).length; j++) {
+            let tag = document.createElement("div");
+            tag.append(projet.langages[j]);
+            tag.classList.add("tag-langage");
+            tag.classList.add("tag-" + projet.langages[j]);
+            divTags.append(tag);
+        }
+        div_pres_projet.append(divTags);
+
         for (let i = 0; i < 5; i++) {
             let image_projet = projet.images[i];
             let li_carousel = listli_carousel[i];
@@ -30,14 +42,20 @@ fetch('public/js/projets.json')
             footer_carousel.innerHTML = image_projet.nom;
         }
         
-        for (let i = 0; i < Object.keys(projet.liens).length; i++) {
-            let lien = document.createElement('a');
-            lien.setAttribute('href', projet.liens[i].lien);
-            let img = document.createElement('img');
-            img.setAttribute('src', "public/images/lien.png")
-            img.title = projet.liens[i].nom
-            img.classList.add("img-rond-moyen")
-            lien.append(img);
-            liens.append(lien);
+        if (Object.keys(projet.liens).length === 0) {
+            let noliens = document.createElement('p');
+            noliens.innerHTML = "Zut il n'y a pas de liens externes relié à ce projet.";
+            liens.append(noliens);
+        } else {
+            for (let i = 0; i < Object.keys(projet.liens).length; i++) {
+                let lien = document.createElement('a');
+                lien.setAttribute('href', projet.liens[i].lien);
+                let img = document.createElement('img');
+                img.setAttribute('src', "public/images/lien.png")
+                img.title = projet.liens[i].nom
+                img.classList.add("img-rond-moyen")
+                lien.append(img);
+                liens.append(lien);
+            }
         }
     })
